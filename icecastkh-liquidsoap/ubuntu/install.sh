6d215e59973335af5cb321c52c64a79d0f1ebbc8
Lucas Saliés Brum <lucas@archlinux.com.br>
2022-01-29 08:54:02 -0400

    restructuring directories

--- icecastkh-liquidsoap/ubuntu/install.sh
+++ icecastkh-liquidsoap/ubuntu/install.sh
@@ -11,6 +11,24 @@
 ICECAST_VERSION="2.4.0-kh15"
 LIQUIDSOAP_VERSION="2.0.2"
 
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LGRAY='\033[0;37m'
+DGRAY='\033[1;30m'
+LRED='\033[1;31m'
+LGREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LBLUE='\033[1;34m'
+LPURPLE='\033[1;35m'
+LCYAN='\033[1;36m'
+WHITE='\033[1;37m'
+NC='\033[0m' # No Color
+
 if [ "$EUID" -ne 0 ]; then
     echo "Please run as root."
     exit
@@ -25,6 +43,28 @@ fi
 
 export DEBIAN_FRONTEND=noninteractive
 
+printf "${PURPLE}************************************************\n"
+printf "${GREEN} ___                       _           \n"
+printf "${GREEN}|_ _|___ ___  ___ __ _ ___| |_     _   \n"
+printf "${GREEN} | |/ __/ _ \/ __/ _\` / __| __|  _| |_ \n"
+printf "${GREEN} | | (_|  __/ (_| (_| \__ \ |_  |_   _|\n"
+printf "${GREEN}|___\___\___|\___\__,_|___/\__|   |_|  \n"
+printf "\n"                                     
+printf "${RED} _     _             _     _ ____                    \n"
+printf "${RED}| |   (_) __ _ _   _(_) __| / ___|  ___   __ _ _ __  \n"
+printf "${RED}| |   | |/ _\` | | | | |/ _\` \___ \ / _ \ / _\` | '_ \ \n"
+printf "${RED}| |___| | (_| | |_| | | (_| |___) | (_) | (_| | |_) |\n"
+printf "${RED}|_____|_|\__, |\__,_|_|\__,_|____/ \___/ \__,_| .__/ \n"
+printf "${RED}            |_|                               |_|    \n"
+printf "\n"
+printf "${YELLOW} ___           _        _ _           \n"
+printf "${YELLOW}|_ _|_ __  ___| |_ __ _| | | ___ _ __ \n"
+printf "${YELLOW} | || '_ \/ __| __/ _\` | | |/ _ \ '__|\n"
+printf "${YELLOW} | || | | \__ \ || (_| | | |  __/ |   \n"
+printf "${YELLOW}|___|_| |_|___/\__\__,_|_|_|\___|_|   \n"
+printf "${PURPLE}************************************************${NC}\n"                                     
+
+
 nginx_tpl="$(curl -s -L https://raw.githubusercontent.com/sistematico/server-scripts/main/icecastkh-liquidsoap/common/stubs/etc/nginx/sites-available/nginx.conf)"
 icecast_service_tpl="$(curl -s -L https://raw.githubusercontent.com/sistematico/server-scripts/main/icecastkh-liquidsoap/common/stubs/etc/systemd/system/icecast-kh.service)"
 liquidsoap_service_tpl="$(curl -s -L https://raw.githubusercontent.com/sistematico/server-scripts/main/icecastkh-liquidsoap/common/stubs/etc/systemd/system/liquidsoap.service)"
@@ -33,9 +73,11 @@ icecast_tpl="https://raw.githubusercontent.com/sistematico/server-scripts/main/i
 radio_tpl="https://raw.githubusercontent.com/sistematico/server-scripts/main/icecastkh-liquidsoap/common/stubs/etc/liquidsoap/radio.liq"
 youtube_tpl="https://raw.githubusercontent.com/sistematico/server-scripts/main/icecastkh-liquidsoap/common/stubs/etc/liquidsoap/youtube.liq"
 
+printf "${PURPLE}*${NC} Updating & Upgrading system...\n"
 apt update -y -q &> /dev/null
 apt upgrade -y -q &> /dev/null
 
+printf "${PURPLE}*${NC} Installing required dependencies...\n"
 apt install -y -q build-essential pkg-config opam \
                 libpcre3-dev libxml2-dev libxslt1-dev libcurl4-openssl-dev \ 
                 libvorbis-dev libmp3lame-dev libmad0-dev libtheora-dev \ 
@@ -48,11 +90,13 @@ opam install sedlex pcre menhir menhirLib dtools duppy mm ssl camomile vorbis la
 opam update -y 1> /dev/null 2> /dev/null
 opam upgrade -y 1> /dev/null 2> /dev/null
 
+printf "${PURPLE}*${NC} Disabling and stopping old systemd units...\n"
 systemctl is-active --quiet liquidsoap && systemctl stop liquidsoap
 systemctl is-active --quiet icecast && systemctl stop icecast
 systemctl is-active --quiet icecast-kh && systemctl stop icecast-kh
 systemctl is-active --quiet nginx && systemctl stop nginx
 
+printf "${PURPLE}*${NC} Creating icecast user...\n"
 pass=$(perl -e 'print crypt($ARGV[0], "password")' "$ICECAST_PW")
 
 if ! id "icecast" &>/dev/null; then
@@ -61,6 +105,7 @@ else
     usermod -m -p "$pass" -d /home/icecast -s /bin/bash -c "Icecast System User" icecast
 fi
 
+printf "${PURPLE}*${NC} Creating liquidsoap user...\n"
 pass=$(perl -e 'print crypt($ARGV[0], "password")' "$LIQUIDSOAP_PW")
 
 if ! id "liquidsoap" &>/dev/null; then
@@ -71,6 +116,7 @@ fi
 
 mkdir -p /var/log/icecast /etc/icecast /etc/liquidsoap /opt/liquidsoap/{playlist,scripts,music} 2> /dev/null
 
+printf "${PURPLE}*${NC} Installing icecast-kh from sources...\n"
 # Icecast KH Build
 if ! command -v icecast &> /dev/null
 then
@@ -84,6 +130,7 @@ then
     make install
 fi
 
+printf "${PURPLE}*${NC} Installing liquidsoap from sources...\n"
 # LiquidSoap Build
 if ! command -v liquidsoap &> /dev/null
 then
@@ -97,6 +144,7 @@ then
     make install
 fi
 
+printf "${PURPLE}*${NC} Creating certs...\n"
 cat >/etc/cloudflare.ini <<-EOL
 dns_cloudflare_email = ${CLOUDFLARE_EMAIL}
 dns_cloudflare_api_key = ${CLOUDFLARE_TOKEN}
@@ -119,6 +167,7 @@ fi
 
 [ -L /etc/nginx/sites-enabled/default ] && rm -f /etc/nginx/sites-enabled/default
 
+printf "${PURPLE}*${NC} Creating nginx proxy...\n"
 printf "$nginx_tpl" | sed -e "s|STREAM_URL|$STREAM_URL|" > /etc/nginx/sites-available/${STREAM_URL}
 
 ln -sf /etc/nginx/sites-available/${STREAM_URL} /etc/nginx/sites-enabled/${STREAM_URL}
@@ -129,6 +178,7 @@ cat >/etc/tmpfiles.d/liquidsoap.conf <<EOL
 f /run/liquidsoap.pid 0644 liquidsoap liquidsoap
 EOL
 
+printf "${PURPLE}*${NC} Creating systemd units...\n"
 printf "$icecast_service_tpl" > /etc/systemd/system/icecast-kh.service
 printf "$liquidsoap_service_tpl" > /etc/systemd/system/liquidsoap.service
 
@@ -137,12 +187,10 @@ curl -sL "$radio_tpl" | sed -e "s|SOURCE_PASSWD|$SOURCE_PASSWD|" -e "s|STREAM_FO
 
 printf "$cron_tpl" > /opt/liquidsoap/scripts/cron.sh
 
-# curl -sLo /opt/liquidsoap/music/house-of-the-rising.mp3 'https://ia601601.us.archive.org/14/items/78_house-of-the-rising-sun_josh-white-and-his-guitar_gbia0001628b/_78_house-of-the-rising-sun_josh-white-and-his-guitar_gbia0001628b_01_3.8_CT_EQ.mp3'
-# curl -sLo /opt/liquidsoap/music/1949-Hitz.mp3 'https://ia800609.us.archive.org/25/items/1949Hitz1/1949%20Hitz%20%23%201.mp3'
-
+printf "${PURPLE}*${NC} Downloading samples...\n"
 [ ! -f '/opt/liquidsoap/music/Chico Rose x 71 Digits – Somebody is Watching Me.mp3' ] && \ 
     curl -sLo '/opt/liquidsoap/music/Chico Rose x 71 Digits – Somebody is Watching Me.mp3' 'https://drive.google.com/uc?export=download&id=1y0xNhh7xljd2453Q-vCZshfw7ncjJ3eW'
-    
+
 [ ! -f '/opt/liquidsoap/music/DubDogz - Baila Conmigo.mp3' ] && \ 
     curl -sLo '/opt/liquidsoap/music/DubDogz - Baila Conmigo.mp3' 'https://drive.google.com/uc?export=download&id=1JeJA3LiEZdvi-Mg-UAVCxluv0oAGWvPR'
 
@@ -164,10 +212,12 @@ fi
 
 [ ! -d /usr/share/liquidsoap/1.4.1 ] && mkdir /usr/share/liquidsoap/1.4.1
 
+printf "${PURPLE}*${NC} Running first cron job(playlists)...\n"
 /bin/bash /opt/liquidsoap/scripts/cron.sh main
 
 touch /var/log/icecast.log /var/log/liquidsoap.log
 
+printf "${PURPLE}*${NC} Fixing permissions...\n"
 chown -R icecast:icecast /var/log/icecast /usr/share/icecast /etc/icecast /var/log/icecast.log
 chown -R liquidsoap:liquidsoap /etc/liquidsoap /opt/liquidsoap /var/log/liquidsoap.log
 
