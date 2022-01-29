0f39a7e0566fef735f104e69c2d46c88c81386a1
Lucas Saliés Brum <lucas@archlinux.com.br>
2022-01-29 12:48:45 -0400

    Automatic update

--- icecast-liquidsoap/ubuntu/install.sh
+++ icecast-liquidsoap/ubuntu/install.sh
@@ -63,13 +63,13 @@ printf "${PURPLE}************************************************${NC}\n"
 printf "\n"
 
 
-nginx_tpl="$(curl -s -L https://raw.githubusercontent.com/sistematico/server-scripts/main/icecastkh-liquidsoap/common/stubs/etc/nginx/sites-available/nginx.conf)"
-icecast_service_tpl="$(curl -s -L https://raw.githubusercontent.com/sistematico/server-scripts/main/icecastkh-liquidsoap/common/stubs/etc/systemd/system/icecast-kh.service)"
-liquidsoap_service_tpl="$(curl -s -L https://raw.githubusercontent.com/sistematico/server-scripts/main/icecastkh-liquidsoap/common/stubs/etc/systemd/system/liquidsoap.service)"
-cron_tpl="$(curl -s -L https://raw.githubusercontent.com/sistematico/server-scripts/main/icecastkh-liquidsoap/common/stubs/opt/liquidsoap/scripts/cron.sh)"
-icecast_tpl="https://raw.githubusercontent.com/sistematico/server-scripts/main/icecastkh-liquidsoap/common/stubs/etc/icecast/icecast.xml"
-radio_tpl="https://raw.githubusercontent.com/sistematico/server-scripts/main/icecastkh-liquidsoap/common/stubs/etc/liquidsoap/radio.liq"
-youtube_tpl="https://raw.githubusercontent.com/sistematico/server-scripts/main/icecastkh-liquidsoap/common/stubs/etc/liquidsoap/youtube.liq"
+nginx_tpl="$(curl -s -L https://raw.githubusercontent.com/sistematico/server-scripts/main/icecast-liquidsoap/common/stubs/etc/nginx/sites-available/nginx.conf)"
+icecast_service_tpl="$(curl -s -L https://raw.githubusercontent.com/sistematico/server-scripts/main/icecast-liquidsoap/common/stubs/etc/systemd/system/icecast-kh.service)"
+liquidsoap_service_tpl="$(curl -s -L https://raw.githubusercontent.com/sistematico/server-scripts/main/icecast-liquidsoap/common/stubs/etc/systemd/system/liquidsoap.service)"
+cron_tpl="$(curl -s -L https://raw.githubusercontent.com/sistematico/server-scripts/main/icecast-liquidsoap/common/stubs/opt/liquidsoap/scripts/cron.sh)"
+icecast_tpl="https://raw.githubusercontent.com/sistematico/server-scripts/main/icecast-liquidsoap/common/stubs/etc/icecast/icecast.xml"
+radio_tpl="https://raw.githubusercontent.com/sistematico/server-scripts/main/icecast-liquidsoap/common/stubs/etc/liquidsoap/radio.liq"
+youtube_tpl="https://raw.githubusercontent.com/sistematico/server-scripts/main/icecast-liquidsoap/common/stubs/etc/liquidsoap/youtube.liq"
 
 #printf "${PURPLE}*${NC} Updating & Upgrading system...\n"
 #apt update -y -q &> /dev/null
